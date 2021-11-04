ad031984fd0fbe6dab95f6bcaaac9ab3abc30f53
ysh <livefor1212@gmail.com>
2021-11-05 02:18:13 +0900

    프로토타입 버전 OrderControllerV1, RepositoryV1, ServiceV1 commit;

--- src/main/java/ysh/logfinder/app/v1/OrderControllerV1.java
+++ src/main/java/ysh/logfinder/app/v1/OrderControllerV1.java
@@ -4,16 +4,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ysh.logfinder.app.v0.OrderServiceV0;
+import ysh.logfinder.trace.TraceStatus;
+import ysh.logfinder.trace.logtrace.LogTraceV1;
 
 @RestController
 //@RestController = @Controller + @ResponseBody
 @RequiredArgsConstructor
 public class OrderControllerV1 {
     private final OrderServiceV0 orderService;
+    private final LogTraceV1 trace;
+
 
     @GetMapping("/v1/request")
     public String request(String itemId){
-        orderService.orderItem(itemId);
+
+        TraceStatus status = null;
+
+        try{
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+        }catch(Exception e){
+            trace.exception(status,e);
+            throw e;//예외를 꼭 다시 던져주어야 한다.
+        }
+
         return "ok";
     }
 
--- src/main/java/ysh/logfinder/app/v1/OrderRepositoryV1.java
+++ src/main/java/ysh/logfinder/app/v1/OrderRepositoryV1.java
@@ -2,16 +2,31 @@ package ysh.logfinder.app.v1;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import ysh.logfinder.trace.TraceStatus;
+import ysh.logfinder.trace.logtrace.LogTraceV1;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
+    private final LogTraceV1 trace;
+
     public void save(String itemId){
-        //저장로직
-        if(itemId.equals("ex")){
-            throw new IllegalStateException("예외발생!!");
+        TraceStatus status = null;
+
+        try{
+            status = trace.begin("OrderRepository.save()");
+
+            //저장로직
+            if(itemId.equals("ex")){
+                throw new IllegalStateException("예외발생!");
+            }
+            sleep(1000);
+
+            trace.end(status);
+        }catch(Exception e){
+            trace.exception(status,e);
+            throw e;//예외를 꼭 다시 던져주어야 한다.
         }
-        sleep(1000);
     }
 
     private void sleep(int millis) {
