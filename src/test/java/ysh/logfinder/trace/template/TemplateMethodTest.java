657bb31650ff5c8a29eac471616e314ce621a3f3
ysh <livefor1212@gmail.com>
2021-11-17 00:05:15 +0900

    템플릿 메서드 패턴 예제2,3

--- src/test/java/ysh/logfinder/trace/template/TemplateMethodTest.java
+++ src/test/java/ysh/logfinder/trace/template/TemplateMethodTest.java
@@ -2,6 +2,9 @@ package ysh.logfinder.trace.template;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import ysh.logfinder.trace.template.code.AbstractTemplate;
+import ysh.logfinder.trace.template.code.SubClassLogic1;
+import ysh.logfinder.trace.template.code.SubClassLogic2;
 
 @Slf4j
 public class TemplateMethodTest {
@@ -30,4 +33,38 @@ public class TemplateMethodTest {
         long resultTime = endTime - startTime;
         log.info("resultTime = {}",resultTime);
     }
+
+    /*
+    템플릿  메서드 패턴 적용
+     */
+    @Test
+    void templateMethodV1(){
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    @Test
+    void templateMethodV2(){
+        AbstractTemplate template1 = new AbstractTemplate(){
+            @Override
+            protected void call() {
+                log.info("비지니스 로직 1 실행");
+            }
+        };
+        log.info("클래스 이름1={}",template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate(){
+            @Override
+            protected void call() {
+                log.info("비지니스 로직 2 실행");
+            }
+        };
+        log.info("클래스 이름2={}",template2.getClass());
+        template2.execute();
+    }
+
 }
