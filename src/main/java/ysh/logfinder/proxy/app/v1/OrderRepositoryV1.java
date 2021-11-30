015d09832e06d986a087900261bcf761bb78a0fc
ysh <livefor1212@gmail.com>
2021-11-30 23:51:57 +0900

    프록시 패키지 추가

---
+++ src/main/java/ysh/logfinder/proxy/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,5 @@
+package ysh.logfinder.proxy.app.v1;
+
+public interface OrderRepositoryV1 {
+    void save(String itemId);
+}
---
+++ src/main/java/ysh/logfinder/proxy/app/v1/OrderService1.java
@@ -0,0 +1,5 @@
+package ysh.logfinder.proxy.app.v1;
+
+public interface OrderService1 {
+    void orderItem(String itemId);
+}
