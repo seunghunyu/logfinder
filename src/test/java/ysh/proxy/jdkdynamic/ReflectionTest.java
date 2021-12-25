305e25f46065860875991a26563297b2abe96c6b
ysh <livefor1212@gmail.com>
2021-12-26 02:00:59 +0900

    Reflection Test2 add

--- src/test/java/ysh/proxy/jdkdynamic/ReflectionTest.java
+++ src/test/java/ysh/proxy/jdkdynamic/ReflectionTest.java
@@ -3,6 +3,8 @@ package ysh.proxy.jdkdynamic;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 @Slf4j
 public class ReflectionTest {
     @Test
@@ -20,6 +22,35 @@ public class ReflectionTest {
         log.info("result={}", result1);
         //공통 로직1 종료
     }
+    @Test
+    void reflection1() throws Exception{
+        //클래스정보
+        Class classHello = Class.forName("ysh.proxy.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+        //class A 정보
+        Method methodClassA = classHello.getMethod("callA");
+        dynamicCall(methodClassA,target);
+        /*
+        Object result1 = methodClassA.invoke(target);
+        log.info("result1 = {}",result1);
+        */
+
+        //class B 정보
+        Method methodClassB = classHello.getMethod("callB");
+        dynamicCall(methodClassA,target);
+        /*
+        Object result2 = methodClassB.invoke(target);
+        log.info("result2 = {}",result2);
+        */
+    }
+
+    private void dynamicCall(Method method, Object target) throws Exception{
+        log.info("start");
+        Object result = method.invoke(target);
+        log.info("result={}",result);
+    }
+
     @Slf4j
     static class Hello{
         public String callA() {
