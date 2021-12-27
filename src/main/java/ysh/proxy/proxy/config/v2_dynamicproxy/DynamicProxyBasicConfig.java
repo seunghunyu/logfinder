62d8cf53fd43f5417461ceb925084d34936c18bc
ysh <livefor1212@gmail.com>
2021-12-28 00:27:19 +0900

    jdk 동적 프록시 적용1_2

---
+++ src/main/java/ysh/proxy/proxy/config/v2_dynamicproxy/DynamicProxyBasicConfig.java
@@ -0,0 +1,37 @@
+package ysh.proxy.proxy.config.v2_dynamicproxy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ysh.logfinder.trace.logtrace.LogTrace;
+import ysh.proxy.proxy.app.v1.*;
+import ysh.proxy.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
+
+import java.lang.reflect.Proxy;
+
+@Configuration
+public class DynamicProxyBasicConfig {
+    @Bean
+    public OrderControllerV1 orderControllerV1(LogTrace logTrace){
+        OrderControllerV1 orderControllerV1 = new OrderControllerV1Impl(orderServiceV1(logTrace));
+        OrderControllerV1 proxy = (OrderControllerV1) Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(),
+                new Class[]{OrderControllerV1.class},
+                new LogTraceBasicHandler(orderControllerV1, logTrace));
+        return proxy;
+    }
+    @Bean
+    public OrderServiceV1 orderServiceV1(LogTrace logTrace){
+        OrderServiceV1 orderServicV1 = new OrderServiceV1Impl(orderRepositoryV1(logTrace));
+        OrderServiceV1 proxy = (OrderServiceV1) Proxy.newProxyInstance(OrderServiceV1.class.getClassLoader(),
+                new Class[]{OrderServiceV1.class},
+                new LogTraceBasicHandler(orderServicV1, logTrace));
+        return proxy;
+    }
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1(LogTrace logTrace){
+        OrderRepositoryV1 orderRepositoryV1 = new OrderRepositoryV1Impl();
+        OrderRepositoryV1 proxy = (OrderRepositoryV1) Proxy.newProxyInstance(OrderRepositoryV1.class.getClassLoader(),
+                new Class[]{OrderRepositoryV1.class},
+                new LogTraceBasicHandler(orderRepositoryV1, logTrace));
+        return proxy;
+    }
+}
