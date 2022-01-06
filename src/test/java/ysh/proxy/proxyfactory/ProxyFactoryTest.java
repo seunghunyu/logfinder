20b5fb2ca326253c98eb59edc0d3fc8b1ca44116
ysh <livefor1212@gmail.com>
2022-01-07 01:10:00 +0900

    프록시 팩토리 예제2_2

--- src/test/java/ysh/proxy/proxyfactory/ProxyFactoryTest.java
+++ src/test/java/ysh/proxy/proxyfactory/ProxyFactoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.AopUtils;
 import ysh.proxy.common.advice.TimeAdvice;
+import ysh.proxy.common.sevice.ConcreteService;
 import ysh.proxy.common.sevice.ServiceImpl;
 import ysh.proxy.common.sevice.ServiceInterface;
 
@@ -32,6 +33,38 @@ public class ProxyFactoryTest {
     }
     @Test
     @DisplayName("구체클래스만 있으면 CGLIB 사용")
-    void concreteProxy(){}
+    void concreteProxy(){
+        ConcreteService target = new ConcreteService();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ConcreteService proxy = (ConcreteService) proxyFactory.getProxy();
+        log.info("targetClass={}",target.getClass());
+        log.info("proxyClass={}",proxy.getClass());
+
+        proxy.call();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
+    @Test
+    @DisplayName("ProxyTargetClass 옵션을 사용하면 인터페이스가 있어도 CGLIB를 사용하고, 클래스 기반 프록시 사용")
+    void proxyTargetClass(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}",target.getClass());
+        log.info("proxyClass={}",proxy.getClass());
+
+        proxy.save();
+
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isFalse();
+        assertThat(AopUtils.isCglibProxy(proxy)).isTrue();
+    }
+
 
 }
