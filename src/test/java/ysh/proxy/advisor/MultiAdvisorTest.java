58b9a36c73ae850bc6c29e4aeb50005df87bdef1
ysh <livefor1212@gmail.com>
2022-01-13 23:58:19 +0900

    여러 어드바이저 적용

---
+++ src/test/java/ysh/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,12 @@
+package ysh.proxy.advisor;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MultiAdvisorTest {
+    @Test
+    @DisplayName("여러프록시")
+    void multiAdvisorTest1(){
+        //proxy
+    }
+}
