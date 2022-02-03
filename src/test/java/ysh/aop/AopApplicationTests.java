54a8d5b4b25d5855378c7c4f3fc05779fc47f8fd
ysh <livefor1212@gmail.com>
2022-02-04 00:46:32 +0900

    예제 프로젝트 코드 추가_2

---
+++ src/test/java/ysh/aop/AopApplicationTests.java
@@ -0,0 +1,7 @@
+package ysh.aop;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AopApplicationTests {
+}
---
+++ src/test/java/ysh/aop/AopTest.java
@@ -0,0 +1,40 @@
+package ysh.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ysh.aop.order.OrderRepository;
+import ysh.aop.order.OrderService;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo(){
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success(){
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception(){
+        Assertions.assertThatThrownBy(()->orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+
+
+}
