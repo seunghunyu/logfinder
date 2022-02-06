2bf74e15d40c83cc281cb6285f9adef6d85d7554
ysh <livefor1212@gmail.com>
2022-02-07 00:08:32 +0900

    스프링 AOP 구현2 - 포인트 컷 분리

--- src/main/java/ysh/aop/order/aop/AspectV2.java
+++ src/main/java/ysh/aop/order/aop/AspectV2.java
@@ -4,11 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
-public class AspectV1 {
-    @Around("execution(* ysh.aop.order..*(..))")
+public class AspectV2 {
+    //ysh.aop.order 패키지와 하위 패키지
+    @Pointcut("execution(* ysh.aop.order..*(..))") //pointcut expression
+    private void allOrder(){}; //pointcut signature
+
+    @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable{
         log.info("[log] {}",joinPoint.getSignature()); // join point 시그니쳐
         return joinPoint.proceed();
--- src/test/java/ysh/aop/AopTest.java
+++ src/test/java/ysh/aop/AopTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import ysh.aop.order.OrderRepository;
 import ysh.aop.order.OrderService;
+import ysh.aop.order.aop.AspectV1;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class)
 public class AopTest {
     @Autowired
     OrderService orderService;
