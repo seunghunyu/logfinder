1d79051a4c85f85a552a2e171d299af3f790d7fc
ysh <livefor1212@gmail.com>
2022-02-09 01:17:59 +0900

    스프링 AOP 구현3 - 어드바이스 추가_2

--- src/main/java/ysh/aop/order/aop/AspectV3.java
+++ src/main/java/ysh/aop/order/aop/AspectV3.java
@@ -22,4 +22,20 @@ public class AspectV3 {
         log.info("[log] {}",joinPoint.getSignature()); // join point 시그니쳐
         return joinPoint.proceed();
     }
+
+    //hello.aop.order 패키지와 하위 패키지이면서 클래스 이름 패턴이 *Service
+    @Around("allOrder() && allService()")
+    public Object doTransaction(ProceedingJoinPoint joinPoint) throws Throwable{
+        try{
+            log.info("[트랜잭션 시작] {}",joinPoint.getSignature());
+            Object result = joinPoint.proceed();
+            log.info("[트랜잭션 커밋] {}",joinPoint.getSignature());
+            return result;
+        }catch(Exception e){
+            log.info("[트랜잭션 롤백] {}",joinPoint.getSignature());
+            throw e;
+        }finally {
+            log.info("[리소스 릴리즈] {}",joinPoint.getSignature());
+        }
+    }
 }
