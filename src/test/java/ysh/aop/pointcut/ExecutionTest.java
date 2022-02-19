bd5464cdc7b067ba3b20919eb017ffffd7723cca
ysh <livefor1212@gmail.com>
2022-02-19 23:44:27 +0900

    execution_1

--- src/test/java/ysh/aop/pointcut/ExecutionTest.java
+++ src/test/java/ysh/aop/pointcut/ExecutionTest.java
@@ -1,6 +1,7 @@
 package ysh.aop.pointcut;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
@@ -21,6 +22,12 @@ public class ExecutionTest {
     @Test
     void printMethod(){
         log.info("helloMethod={}",helloMethod);
+    }
+
+    @Test
+    void exactMatch(){
+        pointcut.setExpression("execution(public String ysh.aop.member.MemberServiceImpl.hello(String))");
+        Assertions.assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 
 }
