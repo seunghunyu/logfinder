d0f09175d9ebd8f3eb12c1e2cc01647ad902fe8e
ysh <livefor1212@gmail.com>
2022-03-02 00:00:57 +0900

    bean_2

--- src/test/java/ysh/aop/pointcut/BeanTest.java
+++ src/test/java/ysh/aop/pointcut/BeanTest.java
@@ -1,4 +1,32 @@
 package ysh.aop.pointcut;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import ysh.aop.order.OrderService;
+@Slf4j
+@Import(BeanTest.BeanAspect.class)
+@SpringBootTest
 public class BeanTest  {
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    void  success(){
+        orderService.orderItem("itemA");
+    }
+    @Aspect
+    static class BeanAspect{
+        @Around("bean(orderService)||bean(*Repository)")
+        public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[bean] {}",joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+
 }
