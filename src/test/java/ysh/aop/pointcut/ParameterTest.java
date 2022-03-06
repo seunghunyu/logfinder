779ab81374c2442b57506e5665c21d40527b2989
ysh <livefor1212@gmail.com>
2022-03-07 00:11:55 +0900

    매개변수 전달_3

--- src/test/java/ysh/aop/pointcut/ParameterTest.java
+++ src/test/java/ysh/aop/pointcut/ParameterTest.java
@@ -1,14 +1,20 @@
 package ysh.aop.pointcut;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ysh.aop.order.aop.member.MemberService;
+import ysh.aop.order.aop.member.annotation.ClassAop;
+import ysh.aop.order.aop.member.annotation.MethodAop;
+
+import java.lang.reflect.Method;
 
 @Slf4j
 @SpringBootTest
@@ -30,14 +36,42 @@ public class ParameterTest {
         @Around("allMember()")
         public Object logArgs1(ProceedingJoinPoint joinPoint) throws Throwable {
             Object arg1 = joinPoint.getArgs()[0];
-            log.info("[logArgs1]{}, args={}", joinPoint.getSignature(), arg1);
+            log.info("[logArgs1]{}, arg={}", joinPoint.getSignature(), arg1);
             return joinPoint.proceed();
         }
 
         @Around("allMember() && args(arg, ..)")
         public Object logArgs2(ProceedingJoinPoint joinPoint, Object arg) throws Throwable {
-            log.info("[logArgs2]{}, args={}", joinPoint.getSignature(), arg);
+            log.info("[logArgs2]{}, arg={}", joinPoint.getSignature(), arg);
             return joinPoint.proceed();
+        }
+
+        @Before("allMember() && args(arg, ..)")
+        public void logArgs3(ProceedingJoinPoint joinPoint, Object arg) throws Throwable {
+            log.info("[logArgs3]{}, arg={}", arg);
+        }
+
+        @Before("allMember() && this(obj)")
+        public void thisArgs(JoinPoint joinPoint, MemberService obj) throws Throwable {
+            log.info("[this]{}, obj={}", joinPoint.getSignature(), obj.getClass());
+        }
+
+        @Before("allMember() && target(obj)")
+        public void targetArgs(JoinPoint joinPoint, MemberService obj) throws Throwable {
+            log.info("[target]{}, obj={}", joinPoint.getSignature(), obj.getClass());
+        }
+
+        @Before("allMember() && @target(annotation)")
+        public void atTarget(JoinPoint joinPoint, ClassAop annotation) throws Throwable {
+            log.info("[target]{}, obj={}", joinPoint.getSignature(), annotation);
+        }
+        @Before("allMember() && @within(annotation)")
+        public void atWithin(JoinPoint joinPoint, ClassAop annotation) throws Throwable {
+            log.info("[within]{}, obj={}", joinPoint.getSignature(), annotation);
+        }
+        @Before("allMember() && @annotation(annotation)")
+        public void atAnnotation(JoinPoint joinPoint, MethodAop annotation) throws Throwable {
+            log.info("[annotation]{}, annotationValue={}", joinPoint.getSignature(), annotation.value());
         }
     }
 
