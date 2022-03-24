bbd18513a90c9cbb1e0b79b6a23a6e9032a376a0
ysh <livefor1212@gmail.com>
2022-03-25 01:11:49 +0900

    의존관계 주입

---
+++ src/test/java/ysh/aop/proxyvs/ProxyDITest.java
@@ -0,0 +1,29 @@
+package ysh.aop.proxyvs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import ysh.aop.order.aop.member.MemberService;
+import ysh.aop.order.aop.member.MemberServiceImpl;
+import ysh.aop.proxyvs.code.ProxyDIAspect;
+
+@Slf4j
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"})  // CGLIB 프록시
+@Import(ProxyDIAspect.class)
+public class ProxyDITest {
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl;
+
+    @Test
+    void go(){
+        log.info("memberService class={}", memberService.getClass());
+        log.info("memberServiceImpl class = {}", memberServiceImpl.getClass());
+        memberServiceImpl.Hello("hello");
+    }
+}
